2a5a6aa3eee2aae00c11a5587c7f81542e5f854e
19Saish <saishpatil1909@gmail.com>
2024-09-12 12:12:42 +0530

    logic complete

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -83,25 +83,45 @@ const Form = () => {
     setEducation(updatedEducation);
   };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(data);
-    console.log(work);
-    console.log(education);
-  };
-
-  const convertToCSV = (data) => {
+  const convertToCSV = (data, work, education) => {
     const csvRows = [];
-    
-    // Headers (keys)
-    const headers = Object.keys(data);
-    csvRows.push(headers.join(","));
 
-    // Values (data)
-    const values = Object.values(data);
-    csvRows.push(values.join(",")); 
+    csvRows.push(
+      "FirstName, MiddleName, LastName, birthDate, Email, Mobile, Aadhar, PAN, Address, City, State, Country, Gender, Marital, Job, Company, Location, WorkStart, WorkEnd, Degree, Specialization, University, Grade, EducationStart, EducationEnd"
+    );
+
+    const maxRows = Math.max(work.length, education.length);
+
+    for (let i = 0; i < maxRows; i++) {
+      const workeach = work[i]
+      const educationeach = education[i]
+
+
+      csvRows.push(
+        `${data.firstName}, ${data.middleName}, ${data.lastName}, ${data.birthDate}, ${data.email}, ${data.mobile}, ${data.aadhar}, ${data.pan}, ${data.address}, ${data.city}, ${data.state}, ${data.country}, ${data.gender}, ${data.martial}, ${workeach.job}, ${workeach.company}, ${workeach.loc}, ${workeach.workStart}, ${workeach.workEnd}, ${educationeach.degree}, ${educationeach.special}, ${educationeach.university}, ${educationeach.grade}, ${educationeach.eduStart}, ${educationeach.eduEnd}`
+      );
+    }
 
     return csvRows.join("\n");
+  };
+
+  const downloadCSV = (csvData) => {
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.setAttribute("hidden", "");
+    a.setAttribute("href", url);
+    a.setAttribute("download", `${data.firstName} ${data.lastName}.csv`);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const csvData = convertToCSV(data, work, education);
+    downloadCSV(csvData)
+    // console.log(work[0].job);
   };
 
   return (
